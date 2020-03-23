c537f2420ee88f4d368bac8417c5c16bc43c1cd6
Fuse Koda <emmanuelfrimpong07@gmail.com>
2020-03-23 22:31:19 +0000

    The Informant

---
+++ app/src/main/java/fuse/koda/theinformant/HomeFragments/NewCase.java
@@ -0,0 +1,32 @@
+package fuse.koda.theinformant.HomeFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fuse.koda.theinformant.R;
+
+
+public class NewCase extends Fragment {
+
+    public NewCase() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_case, container, false);
+    }
+}
--- app/src/main/java/fuse/koda/theinformant/MainActivity.java
+++ app/src/main/java/fuse/koda/theinformant/MainActivity.java
@@ -1,14 +1,103 @@
 package fuse.koda.theinformant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.sdsmdg.harjot.vectormaster.VectorMasterView;
+import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private NavigationAnimation mView;
+    private VectorMasterView heartVector;
+    private VectorMasterView heartVector1;
+    private VectorMasterView heartVector2;
+    private VectorMasterView heartVector3;
+    PathModel outline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mView = findViewById(R.id.customBottomBar);
+        heartVector = findViewById(R.id.home);
+        heartVector1 = findViewById(R.id.add);
+        heartVector2 = findViewById(R.id.contact);
+        heartVector3 = findViewById(R.id.person);
+        mView.setSelectedItemId(R.id.nav_home);
+
+        mView.setOnNavigationItemSelectedListener(MainActivity.this);
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                //tet(6);
+                // find the correct path using name
+               // mlinId.setX(mView.mFirstCurveControlPoint1.x );
+                heartVector.setVisibility(View.VISIBLE);
+                heartVector1.setVisibility(View.GONE);
+                heartVector2.setVisibility(View.GONE);
+                heartVector3.setVisibility(View.GONE);
+                selectAnimation(heartVector);
+                break;
+            case R.id.nav_add:
+               // tet(2);
+                //mlinId.setX(mView.mFirstCurveControlPoint1.x );
+                heartVector.setVisibility(View.GONE);
+                heartVector1.setVisibility(View.VISIBLE);
+                heartVector2.setVisibility(View.GONE);
+                heartVector3.setVisibility(View.GONE);
+
+                selectAnimation(heartVector1);
+                break;
+            case R.id.nav_contact:
+                //mlinId.setX(mView.mFirstCurveControlPoint1.x ) ;
+                heartVector.setVisibility(View.GONE);
+                heartVector1.setVisibility(View.GONE);
+                heartVector2.setVisibility(View.VISIBLE);
+                heartVector3.setVisibility(View.GONE);
+                selectAnimation(heartVector2);
+                break;
+            case R.id.nav_profile:
+                //tet();
+                //mlinId.setX(mView.mFirstCurveControlPoint1.x ) ;
+                heartVector.setVisibility(View.GONE);
+                heartVector1.setVisibility(View.GONE);
+                heartVector2.setVisibility(View.GONE);
+                heartVector3.setVisibility(View.VISIBLE);
+
+                selectAnimation(heartVector3);
+                break;
+        }
+
+        return true;
+    }
+
+    private void selectAnimation(final VectorMasterView heartVector) {
+        outline = heartVector.getPathModelByName("outline");
+        outline.setStrokeColor(Color.parseColor("#ffffff"));
+        outline.setTrimPathEnd(0.0f);
+        // initialise valueAnimator and pass start and end float values
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        valueAnimator.setDuration(1000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                // set trim end value and update view
+                outline.setTrimPathEnd((Float) valueAnimator.getAnimatedValue());
+                heartVector.update();
+            }
+        });
+        valueAnimator.start();
+    }
+
 }
